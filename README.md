b6a9ce7b38a9da13d1b74d65c51748daca05222a
Ryabov Nikolay <149957317+kolysssssiiiikkkk@users.noreply.github.com>
2024-05-29 21:51:33 +0500

    коммитт

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# practica333
+Мой бэкенд: https://pindie.back.nomoredomainswork.ru
+ссылка на репозиторий backend`a : https://github.com/cocka67/pindie-backend
---
+++ app/api/api-hooks.js
@@ -0,0 +1,16 @@
+import {useState, useEffect} from "react";
+import {getNormalizedGamesDataByCategory, isResponseOk} from "./api-utils";
+
+export const useGetDataByCategory = (endpoint, category) => {
+    const [data, setData] = useState(null)
+    useEffect(() => {
+        async function fetchData() {
+            const result = await getNormalizedGamesDataByCategory(endpoint, category)
+            if (isResponseOk(result)) {
+                setData(result)
+            }
+        }
+        fetchData()
+    }, [])
+    return data
+}
